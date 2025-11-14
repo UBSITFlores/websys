168c016dbb1383e7009efe411f8007000abff549
UBSITFlores <168336838+UBSITFlores@users.noreply.github.com>
2025-11-14 22:52:41 +0800

    Login Done | Registration partly Done ( MISSING INPUT OF REQUIREMENTS TO APPLY )

--- functions/account.php
+++ functions/account.php
@@ -1,5 +1,60 @@
 <?php 
 
-//Login and Registration Functions
+    class account{
+        function __construct(){
+            $host = "localhost";
+            $user = "root";
+            $pass = "";
+            $dbname = "portal";
+            $charset = "utf8mb4";
+
+            try{
+                $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+                
+                $connection = new PDO($dsn,$user,$pass);
+                $this->pdo = $connection;
+                
+            }catch(PDOException $e){
+                die("Connection Failed!" . $e->getMessage());
+            }
+        }
+
+        public function login($account_id,$password){
+            $fetch = $this->pdo->prepare("SELECT * FROM account WHERE account_id= :account_id AND password= :password");
+            $fetch->execute([
+                ':account_id' => $account_id,
+                ':password' => $password 
+            ]);
+            $options = [
+                PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+            $user = $fetch->fetch(PDO::FETCH_ASSOC);
+            if (!$user) {
+                echo "
+                    <script>
+                        alert('Provide Correct Credentials...');
+                        window.location.href = 'index.php';
+                    </script>
+                ";
+            } else {
+                $_SESSION['ACCOUNTID'] = $user['account_id'];
+                $_SESSION['FNAME'] = $user['fname'];
+                $_SESSION['LNAME'] = $user['lname'];
+                $_SESSION['ROLE'] = $user['role'];
+
+                if ($user['role'] == 'management') {
+                    header("Location: ../admin/index.php");
+                } elseif ($user['role'] == 'instructor') {
+                    header("Location: ../instructor/index.php");
+                }
+                else {
+                    header("Location: ../student/index.php");
+                }
+                exit();
+            }
+        }
+    }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php 
-    //Pag open ng localhost/dadada
-    //punta siya ng /account/index.php
+<?php
+session_start();
+if (isset($_SESSION['account_id']) && isset($_SESSION['role'])){
+    if ($_SESSION['role'] == "instructor") {
+        header("Location: #");
+        exit();
+        } 
+    elseif ($_SESSION['role'] == "student"){
+        header("Location: #");
+        exit();
+        }
+    else{
+        header("location: #");{
+        }
+    }
+    } else {
+        header("Location: account/index.php");
+        exit();
+        }
 ?>
